4bc59eee82e9dab84442ecc02862c11c439f6df0
Null_忍 <patataOwOsi@gmail.com>
2024-11-07 23:22:21 -0600

    cambios rarunos xdxd

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { EncabezadoComponent } from '../encabezado/encabezado.component';
-import { CarruselComponent } from '../carrusel/carrusel.component';
 import { FormularioReservacionComponent } from '../formulario-reservacion/formulario-reservacion.component';
 import { SystemService } from '../../service/system.service';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from '../slider/slider.component';
+import { CuartosComponent } from "../cuartos/cuartos.component";
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [
     FormularioReservacionComponent,
-    CarruselComponent,
-    EncabezadoComponent,
     CommonModule,
-    SliderComponent
+    SliderComponent,
+    CuartosComponent,
 ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
